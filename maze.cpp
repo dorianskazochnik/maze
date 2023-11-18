c7d74b8acfa39ead9bd9332d0c8c5fc6a9cad1dc
Dorian <150250181+dorianskazochnik@users.noreply.github.com>
2023-11-18 22:23:56 +0300

    maze_1.0.cpp

--- maze.cpp
+++ maze.cpp
@@ -1,33 +1,45 @@
-﻿using namespace std;
+using namespace std;
 
 #include <iostream>
-#include <map>
 #include <iterator>
 #include <random>
 #include <utility>
+#include <queue>
+#include <map>
+#include <vector>
+#include <stack>
 #include <windows.h>
 
-class Maze : public multimap<pair<int, int>, int>
+class Maze : public multimap<pair<int, int>, pair<int, int> >
 {
 protected:
-    multimap<pair<int, int>, int> maze;
+    multimap<pair<int, int>, pair<int, int> > maze;
+    pair<int, int> entry;
+    pair<int, int> exit;
     int rows;
     int columns;
+    int levels;
 public:
-    Maze(int r = 0, int c = 0): multimap()
+    Maze(int r = 0, int c = 0, int l = 0) : multimap()
     {
         rows = r;
         columns = c;
+        levels = l;
     }
-    Maze(const Maze& m) : multimap(maze)
+    Maze(const Maze& m)
     {
+        maze = m.maze;
         rows = m.rows;
         columns = m.columns;
+        levels = m.levels;
     }
     ~Maze()
     {
-        maze.clear();
+        maze.~multimap();
     }
+
+    pair<int, int> getEntry() { return entry; }
+    pair<int, int> getExit() { return exit; }
 
     bool choose_orientation(int width, int height)
     {
@@ -39,7 +51,47 @@ public:
         return width < height;
     }
 
-    void r_division(pair<int, int> coord, pair<int, int> hw, bool orientation)
+    vector<vector<char> > generate()
+    {
+        //создание и заполнение сетки
+        vector<vector<char> > v(rows * 2 + 1, vector<char>(columns * 2 + 1));
+        for (int i = 0; i < rows * 2 + 1; i++)
+        {
+            for (int j = 0; j < columns  * 2 + 1; j++)
+            {
+                v[i][j] = ' ';
+            }
+        }
+        //генерация внутренних стен
+        r_division(v, make_pair(0, 0), make_pair(rows, columns), choose_orientation(rows, columns));
+        srand(time(0));
+        //генерация входа и выхода
+        entry = make_pair((1 + rand() % (rows * 2)) / 2, (1 + rand() % (columns * 2)) / 2);
+        exit = make_pair(((1 + rand() % (columns * 2)) % (rows * 2)) / 2, ((1 + rand() % (rows * 2)) % (columns * 2)) / 2);
+        for (int i = 0; i < rows * 2 + 1; i++)
+        {
+            if (i == 0 || i == rows * 2)
+                for (int j = 0; j < columns * 2 + 1; j++)
+                    v[i][j] = '#';
+            else
+            {
+                v[i][0] = '#';
+                v[i][columns * 2] = '#';
+            }
+        }
+        v[entry.first * 2][entry.second * 2] = 'x';
+        v[exit.first * 2][exit.second * 2] = '!';
+        //вывод
+        display(v);
+
+        //создание очереди - основной структуры лабиринта
+        construct_maze(v);
+        //output();
+        return v;
+    }
+
+    //генератор уровня лабиринта в виде двумерного поля (вектора)
+    void r_division(vector<vector<char> >& v, pair<int, int> coord, pair<int, int> hw, bool orientation)
     {
         if (hw.first < 2 || hw.second < 2)
             return;
@@ -52,84 +104,195 @@ public:
         {
             if (wall.first != passage.first || wall.second != passage.second)
             {
-                if (maze.size() != 0)
+                if (orientation)
                 {
-                    multimap< pair<int, int>, int>::iterator it = maze.find(wall);
-                    if (it != maze.end())
-                    {
-                        it->second = int(it->second | !(orientation));
-                    }
-                    else
-                    {
-                        maze.insert(make_pair(wall, !(orientation)));
-                    }
+                    v[wall.first * 2][wall.second * 2 + 1] = '#';
+                    if (wall.first != 0)
+                        v[wall.first * 2 - 1][wall.second * 2 + 1] = '#';
                 }
-                else if (maze.size() == 0)
+                else
                 {
-                    maze.insert(make_pair(wall, !(orientation)));
+                    v[wall.first * 2 + 1][wall.second * 2] = '#';
+                    if (wall.second != 0)
+                        v[wall.first * 2 + 1][wall.second * 2 - 1] = '#';
                 }
+                v[wall.first * 2 + 1][wall.second * 2 + 1] = '#';
             }
             wall = make_pair(wall.first | direction.first, wall.second | direction.second);
         }
         pair<int, int> n = coord;
         int width = orientation ? hw.first : wall.first - n.first;
         int height = orientation ? wall.second - n.second : hw.second;
-        r_division(n, make_pair(width, height), choose_orientation(width, height));
+        r_division(v, n, make_pair(width, height), choose_orientation(width, height));
 
         n = make_pair(orientation ? coord.first : wall.first, orientation ? wall.second : coord.second);
         width = orientation ? hw.first : coord.first + hw.first - wall.first;
         height = orientation ? coord.second + hw.second - wall.second : hw.second;
-        r_division(n, make_pair(width, height), choose_orientation(width, height));
+        r_division(v, n, make_pair(width, height), choose_orientation(width, height));
     }
 
-    void display()
+    //вывод лабиринта 2d
+    void display(const vector<vector<char> >& v)
     {
-        vector<vector<char> > v(rows * 2 + 1, vector<char>(columns * 2 + 1));
         for (int i = 0; i < rows * 2 + 1; i++)
         {
             for (int j = 0; j < columns * 2 + 1; j++)
             {
-                v[i][j] = ' ';
-            }
-        }
-        for (multimap<pair<int, int>, int>::iterator it = maze.begin(); it != maze.end(); ++it)
-        {
-            for (auto jt = maze.equal_range(it->first).first; jt != maze.equal_range(it->first).second; ++jt)
-            {
-                if (it->second == 0)
-                {
-                    v[it->first.first * 2][it->first.second * 2 + 1] = '#';
-                    if (it->first.first != 0)
-                        v[it->first.first * 2 - 1][it->first.second * 2 + 1] = '#';
-                }
-                else
-                {
-                    v[it->first.first * 2 + 1][it->first.second * 2] = '#';
-                    if (it->first.second != 0)
-                        v[it->first.first * 2 + 1][it->first.second * 2 - 1] = '#';
-                }
-                v[it->first.first * 2 + 1][it->first.second * 2 + 1] = '#';
-            }
-        }
-        cout << '\n';
-        for (int i = 0; i < rows * 2 - 2; i++)
-        {
-            cout << '#';
-            for (int j = 0; j < columns * 2 - 2; j++)
-            {
                 cout << v[i][j];
             }
-            cout << "# \n";
+            cout << '\n';
         }
+    }
+
+    //превратить из вектора векторов в дерево
+    void construct_maze(vector<vector<char> >& v)
+    {
+        for (int i = 0; i < rows * 2 - 1; i++)
+        {
+            for (int j = 0; j < columns * 2 - 1; j++)
+            {
+                if (v[i][j] == 'x')
+                    v[i][j] = ' ';
+                else if (v[i][j] == '!')
+                    v[i][j] = ' ';
+                if (v[i][j] == ' ' && v[i + 1][j] == ' ' && v[i + 2][j] == ' ' && i % 2 == 0 && j % 2 == 0)
+                {
+                    pair<int, int> f(make_pair(i / 2, j / 2));
+                    pair<int, int> s(make_pair(i / 2 + 1, j / 2));
+                    maze.insert(make_pair(f, s));
+                }
+                if (v[i][j] == ' ' && v[i][j + 1] == ' ' && v[i][j + 2] == ' ' && i % 2 == 0 && j % 2 == 0)
+                {
+                    pair<int, int> f(make_pair(i / 2, j / 2));
+                    pair<int, int> s(make_pair(i / 2, j / 2 + 1));
+                    maze.insert(make_pair(f, s));
+                }
+                if (j >= 2)
+                    if (v[i][j] == ' ' && v[i][j - 1] == ' ' && v[i][j - 2] == ' ' && i % 2 == 0 && j % 2 == 0)
+                    {
+                        pair<int, int> f(make_pair(i / 2, j / 2));
+                        pair<int, int> s(make_pair(i / 2, j / 2 - 1));
+                        maze.insert(make_pair(f, s));
+                    }
+                if (i >= 2)
+                    if (v[i][j] == ' ' && v[i - 1][j] == ' ' && v[i - 2][j] == ' ' && i % 2 == 0 && j % 2 == 0)
+                    {
+                        pair<int, int> f(make_pair(i / 2, j / 2));
+                        pair<int, int> s(make_pair(i / 2 - 1, j / 2));
+                        maze.insert(make_pair(f, s));
+                    }
+            }
+        }
+        v[entry.first * 2][entry.second * 2] = 'x';
+        v[exit.first * 2][exit.second * 2] = '!';
+    }
+
+    void path(vector<vector<char> >& v, vector<pair<int, int> >& used)
+    {
+        for (vector<pair<int, int> >::iterator it = used.begin(); it != used.end(); ++it)
+        {
+            v[it->first * 2][it->second * 2] = '*';
+        }
+        v[entry.first * 2][entry.second * 2] = 'x';
+        v[exit.first * 2][exit.second * 2] = '!';
+    }
+
+    //вывод дерева в виде ребер
+    void output()
+    {
+        for (multimap<pair<int, int>, pair<int, int> >::iterator it = maze.begin(); it != maze.end(); ++it)
+        {
+            cout << "(" << it->first.first << "; " << it->first.second << "), (" << it->second.first << it->second.second << "); \n";
+        }
+    }
+
+    //вспомогательная функция для поиск в глубину
+    void DFS(vector<vector<char> >& v)
+    {
+        vector<vector<int> > used(rows * 2 + 1, vector<int>(columns * 2 + 1, -1));
+        vector<pair<int, int> > res;
+        vector<vector<pair<int, int> > > p(rows * 2 + 1, vector<pair<int, int> >(columns * 2 + 1));
+        bool flag = DFS(used, entry, p);
+        if (!flag)
+            cout << "no escape";
+        pair<int, int> current = exit;
+        while (current != entry && flag)
+        {
+            res.push_back(current);
+            current = p[current.first][current.second];
+        }
+        path(v, res);
+        cout << "\n\nentry = (" << entry.first << "; " << entry.second << "), exit = (" << exit.first << "; " << exit.second << ");\n";
+    }
+
+    //рекурсивный поиск в глубину
+    bool DFS(vector<vector<int> >& used, pair<int, int> current, vector<vector<pair<int, int> > >& p)
+    {
+        if (used[current.first][current.second] == 0)
+            return false;
+        if (current == exit)
+            return true;
+        used[current.first][current.second] = 0;
+        for (multimap<pair<int, int>, pair<int, int> >::iterator it = maze.equal_range(current).first; it != maze.equal_range(current).second; ++it)
+        {
+            if (used[it->second.first][it->second.second] == -1)
+            {
+                DFS(used, it->second, p);
+                if (current == exit)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    //поиск в ширину
+    void BFS(vector<vector<char> >& v)
+    {
+        bool flag = false;
+        queue<pair<int, int> > q;
+        q.push(entry);
+        vector<vector<int> > used (rows * 2 + 1, vector<int> (columns * 2 + 1, -1));
+        vector<vector<pair<int, int> > > p (rows * 2 + 1, vector<pair<int, int> >(columns * 2 + 1));
+        vector<pair<int, int> > res;
+        used[entry.first][entry.second] = 0;
+        while (!q.empty() && !flag)
+        {
+            pair<int, int> current = q.front();
+            q.pop();
+            for (multimap<pair<int, int>, pair<int, int> >::iterator it = maze.equal_range(current).first; it != maze.equal_range(current).second && !flag; ++it)
+            {
+                pair<int, int> neighbor = it->second;
+                if (used[neighbor.first][neighbor.second] == -1)
+                {
+                    q.push(neighbor);
+                    used[neighbor.first][neighbor.second] = used[current.first][current.second] + 1;
+                    p[neighbor.first][neighbor.second] = current;
+                    if (neighbor == exit)
+                        flag = true;
+                }
+            }
+            while (current != entry && flag)
+            {
+                res.push_back(current);
+                current = p[current.first][current.second];
+            }
+        }
+        if (!flag)
+            cout << "no escape";
+        path(v, res);
+        cout << "\n\nentry = (" << entry.first << "; " << entry.second << "), exit = (" << exit.first << "; " << exit.second << ");\n";
     }
 };
 
 int main()
 {
     Maze maze(15, 15);
-    maze.r_division(make_pair(0, 0), make_pair(15, 15), maze.choose_orientation(15, 15));
+    vector<vector<char> > v = maze.generate();
     cout << "\n\n\n ------------ \n\n\n";
-    maze.display();
+    maze.BFS(v);
+    maze.display(v);
+    maze.DFS(v);
+    maze.display(v);
     int k;
     cin >> k;
 }
